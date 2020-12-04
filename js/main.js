0af8d9a7994cdad3766a26c36f88b56db5a6a016
Martina Amore <martina.amore02@gmail.com>
2020-12-04 19:29:34 +0100

    Milestone 4 completa

--- js/main.js
+++ js/main.js
@@ -5,7 +5,7 @@ const app = new Vue ({
     data: {
         movies: [],
         tvShows: [],
-        userSearch: "m",
+        userSearch: "",
     },
     methods: {
         searchMovie: function () {
